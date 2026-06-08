b4aa4708da6a0f0b78471475771c5fdddbf6afb1
onerous2 <mspitsyn@emich.edu>
2026-06-08 18:59:13 +0700

    dag file edited

--- dags/load_staging_dag.py
+++ dags/load_staging_dag.py
@@ -4,15 +4,15 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 
-#Базовые настройки по умолчанию
+# Базовые настройки по умолчанию
 default_args = {
-    'owner' : 'com_developer',
-    'depends_on_past' : False,
-    'start_date' : datetime(2026, 1, 1),
-    'email_on_failure' : False,
-    'email_on_retry' : False,
-    'retries' : 1,
-    'retry_delay' : timedelta(minutes=2),
+    'owner': 'com_developer',
+    'depends_on_past': False,
+    'start_date': datetime(2026, 1, 1),
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=2),
 }
 
 DATA_MAPPING = {
@@ -29,10 +29,10 @@ DATA_MAPPING = {
 
 def load_csv_to_staging(csv_file: str, table_name: str):
     """Высокоскоростная очистка и загрузка CSV в Postgres через COPY expert"""
-    # Имя connection, которое мы прописали в docker-compose.yml
+    # Имя connection, которое мы прописали в Airflow Connections
     pg_hook = PostgresHook(postgres_conn_id='postgres_dwh')
     
-    # 1. Очищаем таблицу перед загрузкой (обеспечиваем идемпотентность)
+    # 1. Очищаем таблицу перед загрузкой
     pg_hook.run(f"TRUNCATE TABLE {table_name};")
     
     # Внутри контейнера Airflow папка ./data/raw примонтирована к /opt/airflow/data/raw
@@ -64,7 +64,7 @@ with DAG(
     'olist_load_staging',
     default_args=default_args,
     description='ЭТАП 1: Извлечение данных из Data Lake и загрузка в Staging слои DWH',
-    schedule_interval=None, # Запускаем вручную для тестов
+    schedule_interval=None,
     catchup=False,
     tags=['olist', 'staging'],
 ) as dag:
@@ -77,5 +77,7 @@ with DAG(
         PythonOperator(
             task_id=task_id,
             python_callable=load_csv_to_staging,
-            op_kwargs={'csv_file': csv_file, table_name: table_name},
+            # ИСПРАВЛЕНО ЗДЕСЬ: 'table_name' теперь строка-ключ
+            op_kwargs={'csv_file': csv_file, 'table_name': table_name},
+            dag=dag,
         )
